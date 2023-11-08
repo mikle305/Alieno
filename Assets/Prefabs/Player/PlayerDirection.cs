ddbbacb001e1fe2b5a644fcf7ac3af5cf4770dd8
Anton Shuklin <87969010+EvilInsideYou@users.noreply.github.com>
2023-11-08 15:48:14 +0300

    Updated player. Updated camera. Added some sci-fi shaders

--- Assets/Prefabs/Player/PlayerDirection.cs
+++ Assets/Prefabs/Player/PlayerDirection.cs
@@ -4,7 +4,7 @@ public class PlayerDirection : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
     [SerializeField] private Transform _playerTransform;
-    
+    [SerializeField] private float _turnSpeed = 0.5f;
     private Vector3 _mouse_pos;
     private Vector3 _playerPos;
     private float _angle;
@@ -16,12 +16,25 @@ public class PlayerDirection : MonoBehaviour
     
     private void UpdatePlayerDirection()
     {
+        GetAngle();
+
+        SetAngleWithSmooth();
+    }
+
+    private void SetAngleWithSmooth()
+    {
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, -_angle + 90, 0));
+
+        _playerTransform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * _turnSpeed);
+    }
+
+    private void GetAngle()
+    {
         _mouse_pos = Input.mousePosition;
         _mouse_pos.z = 5.23f; //The distance between the camera and object
         _playerPos = _camera.WorldToScreenPoint(_playerTransform.position);
         _mouse_pos.x = _mouse_pos.x - _playerPos.x;
         _mouse_pos.y = _mouse_pos.y - _playerPos.y;
         _angle = Mathf.Atan2(_mouse_pos.y, _mouse_pos.x) * Mathf.Rad2Deg;
-        _playerTransform.rotation = Quaternion.Euler(new Vector3(0, -_angle + 90, 0));
     }
 }
