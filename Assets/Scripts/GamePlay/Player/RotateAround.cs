5baccea8eb65f4eb18d1e999024abe1dfdd49432
Anton Shuklin <87969010+EvilInsideYou@users.noreply.github.com>
2023-11-14 17:55:19 +0300

    Added visuals to level map

---
+++ Assets/Scripts/GamePlay/Player/RotateAround.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] private float _speed = 1f;
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, Vector3.up, _speed * Time.deltaTime);
+    }
+}
