dd18beb1c50dacfd5eb0033ab01ff556141c8093
Anton Shuklin <87969010+EvilInsideYou@users.noreply.github.com>
2023-11-21 18:47:32 +0300

    Added some basic enemies

--- Assets/Scripts/GamePlay/Player/PlayerDash.cs
+++ Assets/Scripts/GamePlay/Player/PlayerDash.cs
@@ -94,7 +94,6 @@ namespace GamePlay.Player
         {
             GameObject go = new GameObject("Trail");
             go.transform.SetPositionAndRotation(_playerBody.position,_playerBody.rotation);
-
             MeshRenderer mr = go.AddComponent<MeshRenderer>();
             MeshFilter mf = go.AddComponent<MeshFilter>();
 
