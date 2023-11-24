91a052fddbd239cf01b4b25beb08e890ae76e8f7
mikle305 <54461515+mikle305@users.noreply.github.com>
2023-11-24 12:57:39 +0300

    Added gizmo for enemy spawn

---
+++ Assets/Scripts/Editor/EnemySpawnEditor.cs
@@ -0,0 +1,17 @@
+﻿using GamePlay.Enemy;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(EnemySpawn))]
+    public class EnemySpawnerEditor : UnityEditor.Editor
+    {
+        [DrawGizmo(GizmoType.NotInSelectionHierarchy | GizmoType.Active | GizmoType.Selected)]
+        public static void RenderCustomGizmo(EnemySpawn enemySpawner, GizmoType gizmo)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(enemySpawner.transform.position, 0.5f);
+        }
+    }
+}
