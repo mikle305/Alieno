b827872bf50d3b6c8e9bcac82fffda151506feed
mikle305 <54461515+mikle305@users.noreply.github.com>
2023-11-27 19:13:50 +0300

    Made ui logic with test buttons for abilities selection

---
+++ Assets/Scripts/UI/GamePlay/AbilityButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GamePlay.Abilities;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.GamePlay
+{
+    public class AbilityButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [field: SerializeField] public TextMeshProUGUI Text { get; private set; }
+        
+        public AbilityId AbilityId { get; set; }
+
+        public event Action<AbilityButton> ButtonClicked;
+
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(InvokeButtonClicked);
+        }
+
+        private void InvokeButtonClicked() 
+            => ButtonClicked?.Invoke(this);
+    }
+}
---
+++ Assets/Scripts/UI/GamePlay/AbilitySelectionView.cs
@@ -0,0 +1,39 @@
+﻿using GamePlay.Abilities;
+using Services;
+using UnityEngine;
+
+namespace UI.GamePlay
+{
+    public class AbilitySelectionView : MonoBehaviour
+    {
+        [SerializeField] private AbilityButton[] _buttons;
+        
+        private AbilitySelectionService _abilitySelectionService;
+        
+        
+        private void Start()
+        {
+            _abilitySelectionService = AbilitySelectionService.Instance;
+            _abilitySelectionService.AbilitiesGenerated += OnAbilitiesGenerated;
+        }
+
+        private void OnAbilitiesGenerated(AbilityId[] abilities)
+        {
+            for (var i = 0; i < abilities.Length; i++)
+            {
+                _buttons[i].AbilityId = abilities[i];
+                _buttons[i].Text.text = abilities[i].ToString();
+                _buttons[i].ButtonClicked += OnButtonClicked;
+                _buttons[i].gameObject.SetActive(true);
+            }
+        }
+
+        private void OnButtonClicked(AbilityButton abilityButton)
+        {
+            foreach (AbilityButton button in _buttons) 
+                button.ButtonClicked -= OnButtonClicked;
+            
+            _abilitySelectionService.SetSelectedAbility(abilityButton.AbilityId);
+        }
+    }
+}
