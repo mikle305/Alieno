d095d13a369e887349cca64cec87bcd4f77b546e
mikle305 <54461515+mikle305@users.noreply.github.com>
2023-11-27 14:10:36 +0300

    Fixed poison and flame stack bug

--- Assets/Scripts/Services/Damage/StatusHandlers/FlameHandler.cs
+++ Assets/Scripts/Services/Damage/StatusHandlers/FlameHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using GamePlay.Characteristics;
 using GamePlay.Damage;
@@ -7,6 +8,9 @@ namespace Services.Damage
 {
     public class FlameHandler : StatusHandler<FlameStatus>
     {
+        private readonly Dictionary<HealthData, CancellationTokenSource> _currentReceivers = new();
+        
+        
         protected override bool OnHandle(DamageData damageData, FlameStatus status)
         {
             StartDamageLoop(damageData, status).Forget();
@@ -16,20 +20,25 @@ namespace Services.Damage
         private async UniTask StartDamageLoop(DamageData damageData, FlameStatus status)
         {
             HealthData receiverHealth = damageData.Receiver;
-            if (receiverHealth == null)
+            if (receiverHealth == null || receiverHealth.Current == 0)
                 return;
 
-            var cts = new CancellationTokenSource();
-            receiverHealth.ZeroReached += cts.Cancel;
-            while (!cts.IsCancellationRequested && status.CountLeft != 0)
+            if (_currentReceivers.TryGetValue(receiverHealth, out CancellationTokenSource oldTokenSource))
+                oldTokenSource.Cancel();
+
+            var newTokenSource = new CancellationTokenSource();
+            _currentReceivers[receiverHealth] = newTokenSource;
+            receiverHealth.ZeroReached += newTokenSource.Cancel;
+            
+            while (!newTokenSource.IsCancellationRequested && status.CountLeft != 0)
             {
                 status.CountLeft--;
                 float damage = damageData.MainDamage * status.DamagePercents / 100; 
                 receiverHealth.Decrease(damage);
-                await UniTask.WaitForSeconds(status.Rate, cancellationToken: cts.Token);
+                await UniTask.WaitForSeconds(status.Rate, cancellationToken: newTokenSource.Token);
             }
 
-            receiverHealth.ZeroReached -= cts.Cancel;
+            receiverHealth.ZeroReached -= newTokenSource.Cancel;
         }
     }
 }
