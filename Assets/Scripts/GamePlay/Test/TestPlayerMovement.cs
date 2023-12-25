4b32493d7a36f05d208b1049aeb1d20b0cc5a3f0
mikle305 <54461515+mikle305@users.noreply.github.com>
2023-12-25 12:09:02 +0300

    Added movement-ai lib

---
+++ Assets/Scripts/GamePlay/Test/TestPlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace GamePlay.Test
+{
+    public class TestPlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _body;
+        [SerializeField] private float _runSpeed = 5.0f;
+        
+        private Vector2 _movementDirection;
+
+
+        private void Update()
+        {
+            _movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        }
+
+        private void FixedUpdate()
+        {
+            if (_movementDirection != Vector2.zero)
+                Move(_movementDirection);
+            else
+                Stop();
+        }
+
+        private void Move(Vector2 direction) 
+            => _body.velocity = new Vector3(x: direction.x * _runSpeed, y: 0, z: direction.y * _runSpeed);
+
+        private void Stop() 
+            => _body.velocity = Vector3.zero;
+    }
+}
