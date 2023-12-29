af6f920ed8f1533ce82a8fc5e63a0e4ef09aa064
mikle305 <54461515+mikle305@users.noreply.github.com>
2023-12-29 15:10:49 +0300

    Removed prefab world builder

--- Assets/Scripts/GamePlay/Enemy/EnemySpawn.cs
+++ Assets/Scripts/GamePlay/Enemy/EnemySpawn.cs
@@ -6,5 +6,6 @@ namespace GamePlay.Enemy
     public class EnemySpawn : MonoBehaviour
     {
         [field: SerializeField] public EnemyId Id { get; private set; }
+        [field: SerializeField, Min(0)] public float HealthMultiplier { get; private set; } = 1;
     }
 }
--- Assets/Scripts/StaticData/Prefabs/Level/Room.cs
+++ Assets/Scripts/StaticData/Prefabs/Level/Room.cs
@@ -10,7 +10,7 @@ namespace StaticData.Prefabs
         [SerializeField] private Transform _enemySpawnsParent;
         [field: SerializeField] public Transform EntryPoint { get; private set; }
         [field: SerializeField] public GameObject ExitPoint { get; private set; }
-        [field: SerializeField] public MusicId BackgroundMusic { get; private set; }
+        [field: SerializeField] public MusicId BackgroundMusic { get; private set; } = MusicId.Battle1Low;
 
         public EnemySpawn[] EnemySpawns => _enemySpawnsParent.GetChildren<EnemySpawn>();
     }
